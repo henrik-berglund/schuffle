3fe03a8705f499643874e0b85bfd2bc79bc7537d
Henrik Berglund <henrik.berglund@proagile.se>
2023-06-29 18:49:21 +0200

    dropped tiles are removed from rack

--- static/game.js
+++ static/game.js
@@ -2,8 +2,8 @@ let game;
 
 class Game {
     constructor() {
-        this.rack = document.getElementById('rack');
-        this.word = document.getElementById('word');
+        this.letterRackElement = document.getElementById('letter-rack');
+        this.solutionRowElement = document.getElementById('solution-row');
         this.resultElement = document.getElementById('result');
         this.fetchWord();
     }
@@ -12,33 +12,51 @@ class Game {
         fetch('/api/word')
             .then(response => response.json())
             .then(data => {
-                this.solution = data.solutions[0];
-                this.createTiles(data.scrambled);
+                this.scrambledWord = data.scrambled;
+                this.solution = data.solutions[0];  // We take the first solution for simplicity
+                this.renderTiles();
             });
     }
 
-    createTiles(scrambledWord) {
-        // Remove old tiles if they exist
-        while (this.rack.firstChild) {
-            this.rack.removeChild(this.rack.firstChild);
-        }
+    renderTiles() {
+        // Clear existing tiles and dropzones
+        this.letterRackElement.innerHTML = '';
+        this.solutionRowElement.innerHTML = '';
 
-        for (let char of scrambledWord) {
+        // Create new tiles and dropzones
+        for (let i = 0; i < this.scrambledWord.length; i++) {
+            // Create a new tile
             const tile = document.createElement('div');
-            tile.classList.add('tile');
-            tile.textContent = char;
+            tile.textContent = this.scrambledWord[i];
+            tile.id = `tile${i}`;
             tile.draggable = true;
-            tile.ondragstart = this.dragStart;
-            this.rack.appendChild(tile);
-        }
-    }
+            tile.ondragstart = (event) => {
+                event.dataTransfer.setData("text", event.target.id);
+            };
+            tile.className = 'letter';
+            this.letterRackElement.appendChild(tile);
 
-    dragStart(e) {
-        e.dataTransfer.setData('text', e.target.textContent);
+            // Create a new dropzone
+            const dropzone = document.createElement('div');
+            dropzone.ondrop = (event) => {
+                event.preventDefault();
+                const data = event.dataTransfer.getData("text");
+                const letterTile = document.getElementById(data);
+                if (letterTile && !event.target.textContent) {
+                    event.target.appendChild(document.getElementById(data));
+                }
+            };
+            dropzone.ondragover = (event) => {
+                event.preventDefault();
+            };
+            dropzone.className = 'dropzone';
+            this.solutionRowElement.appendChild(dropzone);
+        }
     }
 
     checkWord() {
-        const userWord = Array.from(this.word.children).map(tile => tile.textContent).join('');
+        const userWordArray = Array.from(this.solutionRowElement.children).map(child => child.textContent);
+        const userWord = userWordArray.join('');
         if (userWord === this.solution) {
             this.resultElement.textContent = 'Correct!';
         } else {
@@ -49,19 +67,6 @@ class Game {
 
 window.onload = () => {
     game = new Game();
-
-    const slots = document.getElementsByClassName('slot');
-    for (let slot of slots) {
-        slot.ondragover = e => e.preventDefault();
-        slot.ondrop = e => {
-            if (!e.target.classList.contains('tile')) {
-                const tile = document.createElement('div');
-                tile.classList.add('tile');
-                tile.textContent = e.dataTransfer.getData('text');
-                e.target.appendChild(tile);
-            }
-        };
-    }
 }
 
 function checkAnswer() {
