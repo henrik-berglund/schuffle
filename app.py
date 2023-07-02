e46b3f10c54c2511b428a77d75214fe3aa8b84bb
Henrik Berglund <henrik.berglund@proagile.se>
2023-07-02 20:37:38 +0200

    .

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def get_word():
 def game():
     return send_from_directory(app.static_folder, 'game.html')
 
+@app.route('/bs')
+def bs():
+    return send_from_directory(app.static_folder, 'bs.html')
 
 if __name__ == '__main__':
     app.run(port=8000, host="0.0.0.0", debug=True)
