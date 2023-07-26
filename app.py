706f7ca88caf9a2198f3791980a714cdee0a0708
Henrik Berglund <henrik.berglund@proagile.se>
2023-07-26 19:02:53 +0200

    .

--- app.py
+++ app.py
@@ -50,16 +50,40 @@ def new_move():
     if len(rows) == 1:
         # Played letters are in the same row
         played_letters.sort(key=lambda letter: letter['x'])
+
+        # Check if x positions are in sequence or if there are gaps
+        x_positions = [letter['x'] for letter in played_letters]
+        for i in range(len(x_positions) - 1):
+            if x_positions[i] != x_positions[i+1] - 1:
+                # There is a gap between two letters, check if there is a letter in the grid to cover the gap
+                missing_x = x_positions[i] + 1
+                missing_y = played_letters[0]['y']
+                if grid[missing_y][missing_x] == "":
+                    return jsonify({'message': 'Invalid move. There is a gap between played letters.'}), 400
+
         print("Horizontal Order:", [letter['value'] for letter in played_letters])
     else:
         # Played letters are in the same column
         played_letters.sort(key=lambda letter: letter['y'])
+
+        # Check if y positions are in sequence or if there are gaps
+        y_positions = [letter['y'] for letter in played_letters]
+        for i in range(len(y_positions) - 1):
+            if y_positions[i] != y_positions[i+1] - 1:
+                # There is a gap between two letters, check if there is a letter in the grid to cover the gap
+                missing_y = y_positions[i] + 1
+                missing_x = played_letters[0]['x']
+                if grid[missing_y][missing_x] == "":
+                    return jsonify({'message': 'Invalid move. There is a gap between played letters.'}), 400
+
         print("Vertical Order:", [letter['value'] for letter in played_letters])
 
     # Rest of your code to handle the valid move and return the response
     # ...
 
+    # If no error was found, return a successful response
     return jsonify({'message': 'Move successfully processed.'})
+
 
 
 @app.route('/bs')
