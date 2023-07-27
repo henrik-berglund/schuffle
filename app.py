ebeca2985065af281a800b4feb1e1f2d02661d28
Henrik Berglund <henrik.berglund@proagile.se>
2023-07-27 10:28:38 +0200

    .

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import random
 app = Flask(__name__, static_folder='static')
 CORS(app)  # This will enable CORS for all routes
 
-words = ["sill", "dill", "torsk", "romsås", "kräftor"]
-
 
 @app.route('/api/word', methods=['GET'])
 def get_word():
@@ -44,6 +42,36 @@ def flip_grid(grid):
 
     return flipped_grid
 
+def get_crossing_words(grid, played_letters):
+    words = []
+
+    for letter in played_letters:
+        x, start_y = letter['x'], letter['y']
+        print(f"{x, start_y}")
+        # Follow word upwards until the start is found
+        while 0 < start_y and not is_blank_grid(grid[start_y-1][x]):
+            start_y -= 1
+            print(f"up {x, start_y}")
+
+        # Follow word downwards until the end is found
+        end_y =  letter['y']
+        print(f"down {x, end_y}")
+        while end_y < 14 and not is_blank_grid(grid[end_y+1][x]):
+            end_y += 1
+            print(f"{x, end_y}")
+
+
+        word = ""
+        for y in range(start_y, end_y+1):
+            if y == letter['y']:
+                word += letter['value']
+            else:
+                word += grid[y][x]
+        if  len(word) > 1 :
+            words.append(word)
+
+    return words
+
 def flip_played_letters(played_letters):
     flipped_letters = []
     for letter in played_letters:
@@ -73,6 +101,7 @@ def new_move():
     grid = data['grid']
     played_letters = data['playedLetters']
     post_response = None
+    formed_words = []
 
     # Check if all played letters are in the same row or same column
     rows = set()
@@ -87,7 +116,15 @@ def new_move():
         post_response = jsonify({'message': 'Invalid move. Played letters are not adjacent to any existing letters on the grid.'}), 400
     elif len(rows) == 1: # Played letters are in the same row
         post_response, word = check_and_collect_horizontal_word(grid, played_letters, post_response)
-        print("Horizontal word:", word)
+
+        if not post_response:
+            print("Horizontal word:", word)
+            formed_words.append(word)
+            crossing = get_crossing_words(grid, played_letters)
+            print("Crossing words: ", crossing)
+            formed_words.extend(crossing)
+            print("All words: ", formed_words)
+
     else: # Played letters are in the same column
         grid = flip_grid(grid)
         played_letters = flip_played_letters(played_letters)
@@ -96,6 +133,7 @@ def new_move():
         print("Vertical word:", word)
 
     if not post_response:
+
         post_response = jsonify({'message': 'Move successfully processed.'})
 
     return post_response
